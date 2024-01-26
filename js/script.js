e87123da48c0c62ed13fa3af6a8dcf0061c0e1c5
sannasinne <sanna@keistinen.fi>
2024-01-26 09:34:56 +0200

    Changed and removed values

--- js/script.js
+++ js/script.js
@@ -4,10 +4,14 @@ const countElement = document.querySelector(".count");
 const learning = {};
 
 learning.topic = "JS";
-learning.learningGoals = ["Build JS Programs", "Learning React", "Build beautiful websites", "Coding everyday"];
+learning.learningGoals = ["Build JS Programs", "Learn React", "Build beautiful websites", "Coding everyday"];
 learning.category = "Front End Development";
 learning.topicImportance = "High";
 
 console.log(learning);
 
 learning.topic = "JavaScript";
+
+learning.learningGoals.splice(1, 1);
+
+console.log(learning);
