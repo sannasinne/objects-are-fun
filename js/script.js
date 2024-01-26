18f294c47e6df8b26679460d6f76313c444fc72c
sannasinne <sanna@keistinen.fi>
2024-01-26 09:31:33 +0200

    Created an object and properties with dot notation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs", "Learning React", "Build beautiful websites", "Coding everyday"];
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
